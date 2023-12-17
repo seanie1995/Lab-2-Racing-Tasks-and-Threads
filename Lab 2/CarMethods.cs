b0fdeb3e4609ef0d97d0a8745e0ab177f18e8826
seanie1995 <seanschelin@gmail.com>
2023-12-17 08:51:20 +0100

    Streamlined one of the methods (DriveAsync) and made the program more automatic

--- Lab 2/CarMethods.cs
+++ Lab 2/CarMethods.cs
@@ -51,11 +51,28 @@ namespace Lab_2
             {
                 await Console.Out.WriteLineAsync($"{car.name} finished the race at place: {place + 1}");
             }                           
+            
             if (place == 4)
             {
-                Console.Clear();
-                await Console.Out.WriteLineAsync("Race is over!");
-                await Console.Out.WriteLineAsync("Press [Enter] if results screen doesn't load automatically in a few seconds:");
+                Console.Clear();                
+                await Console.Out.WriteLineAsync("RACE RESULTS!");
+            
+                Console.ForegroundColor= ConsoleColor.Yellow;
+                await Console.Out.WriteLineAsync($"--------------------------------------------");
+            
+                for (int i = 0; i < carPlace.Count; i++)
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    
+                    await Console.Out.WriteLineAsync($"{i + 1}: {carPlace[i].name}");
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    await Console.Out.WriteLineAsync($"--------------------------------------------");
+                }
+                Console.ResetColor();          
+            
+                await Console.Out.WriteLineAsync("The program will exit in 10 seconds");
+                await Task.Delay(10000);
+                Environment.Exit(0);
             }
         }    
         public static async Task CarProblems(Car car)
@@ -91,61 +108,27 @@ namespace Lab_2
         }      
         public static async Task RaceProgress(List<Car> cars, List<Car> carPlace)
         {
-            if (carPlace.Count == 5)
+            
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            await Console.Out.WriteLineAsync($"--------------------------------------------");
+            foreach (Car car in cars)
             {
-                await Task.Delay(1000);
-                await PrintRaceResults(carPlace);               
-            }
-
-            else
-            {
-                Console.ForegroundColor = ConsoleColor.Yellow;
-                await Console.Out.WriteLineAsync($"--------------------------------------------");
-                foreach (Car car in cars)
+                if (car.distanceTravelled != 100)
                 {
-                    if (car.distanceTravelled != 100)
-                    {
-                        await Console.Out.WriteLineAsync($"{car.name}: {car.distanceTravelled}km || Speed: {car.speed} km/h.");
-                    }
-                    else if (car.distanceTravelled == 100)
-                    {
-                        Console.ForegroundColor = ConsoleColor.Green;
-                        await Console.Out.WriteLineAsync($"{car.name} finished!");
-                        Console.ForegroundColor = ConsoleColor.Yellow;
-                    }
-                    await Console.Out.WriteLineAsync($"--------------------------------------------");
+                    await Console.Out.WriteLineAsync($"{car.name}: {car.distanceTravelled}km || Speed: {car.speed} km/h.");
                 }
+                else if (car.distanceTravelled == 100)
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    await Console.Out.WriteLineAsync($"{car.name} finished!");
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                }
+                await Console.Out.WriteLineAsync($"--------------------------------------------");
             }
-           
             Console.ResetColor();
         }
 
-        public static async Task PrintRaceResults(List<Car> carPlace)
-        {
-            Console.Clear();
-            await Console.Out.WriteLineAsync("RACE RESULTS!");
-            
-            Console.ForegroundColor= ConsoleColor.Yellow;
-            await Console.Out.WriteLineAsync($"--------------------------------------------");
-            
-            for (int i = 0; i < carPlace.Count; i++)
-            {
-                Console.ForegroundColor = ConsoleColor.Green;
-                    
-                await Console.Out.WriteLineAsync($"{i + 1}: {carPlace[i].name}");
-                Console.ForegroundColor = ConsoleColor.Yellow;
-                await Console.Out.WriteLineAsync($"--------------------------------------------");
-            }
-            Console.ResetColor();          
-
-            while (true)
-            {
-                await Console.Out.WriteLineAsync("Press enter to quit");
-                Console.ReadKey();
-                Environment.Exit(0);
-            }
-
-        }
+        
 
         
     }
--- Lab 2/Program.cs
+++ Lab 2/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
 using System.Diagnostics.Metrics;
 using System.Timers;
 
@@ -30,25 +31,26 @@ namespace Lab_2
 
             List<Car> carsList = new List<Car> { car1, car2, car3, car4, car5 };
             List<Car> carPlace = new List<Car> { };
-            List<Task> raceTasks = carsList.Select(car => Task.Run(async () => await CarMethods.DriveAsync(car, carPlace))).ToList();
+            List<Task> raceTasks = carsList.Select(car => CarMethods.DriveAsync(car, carPlace)).ToList();
             await Console.Out.WriteLineAsync("Race started!");
-
+            
+            Stopwatch timer = new Stopwatch();
+            timer.Start();
             while (input == "1")
-            {
+            {               
                 await Console.Out.WriteLineAsync("Press enter to see race status:");
                 
                 ConsoleKeyInfo keyInfo = Console.ReadKey();
                 if (keyInfo.Key == ConsoleKey.Enter)
                 {
                     Console.Clear();
-                    await CarMethods.RaceProgress(carsList, carPlace);
-                }              
+                    
+                    await CarMethods.RaceProgress(carsList, carPlace);                   
+                }                            
             }
 
-            Stopwatch timer = new Stopwatch();
-            timer.Start();
-
-            while (input == "2")
+            
+            while (input == "2" && carPlace.Count < carsList.Count)
             {               
                 Console.Clear();
                 Console.ResetColor();
@@ -57,11 +59,10 @@ namespace Lab_2
                     await Console.Out.WriteLineAsync($"{timer.Elapsed.TotalSeconds:F0} seconds passed");
                 }
                 await CarMethods.RaceProgress(carsList, carPlace);
-                Thread.Sleep(5000);
-             
+                
+                Thread.Sleep(5000);        
             }
-
-
+            await Task.WhenAll(raceTasks);
         }               
         
     }
